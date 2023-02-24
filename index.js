f69b0e02bb62b683ffdd5b0b1877aa8eb276eb30
kidereo <kirill.derevenski@gmail.com>
2023-02-24 09:32:10 +0100

    Connected to Atlas.

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const startupDebugger = require("debug")("app:startup");
 const objectDebugger = require("debug")("app:objects");
+const dbDebugger = require("debug")("app:db");
 
+require("dotenv").config();
 const config = require("config");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const auth = require("./middleware/auth");
 const homeRoute = require("./routes/home");
 const sauceRoutes = require("./routes/sauces");
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.DB_USER +
+      ":" +
+      process.env.DB_PASSWORD +
+      process.env.DB_PATH +
+      process.env.DB_NAME +
+      "?retryWrites=true&w=majority"
+  )
+  .then(() => dbDebugger("Database connection sucessfull..."))
+  .catch((error) =>
+    dbDebugger("Database connection failed with message:", error.message)
+  );
 
 app.use(express.json());
 app.use(express.static("public"));
