b132c2bed92d48f2edc9d8251013ac778d5e775f
kidereo <kirill.derevenski@gmail.com>
2023-02-28 13:12:38 +0100

    Refactored user verification and registration.

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 const dbDebugger = require("debug")("app:db");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
-const { User } = require("../models/User");
+const { User, validateUser } = require("../models/User");
 
 /**
  * Get all users
@@ -19,19 +21,37 @@ exports.index = async (req, res) => {
  * Add a user
  */
 exports.store = async (req, res) => {
+  //Validate request's body with Joi
+  const { error } = validateUser(req.body);
+  if (error) {
+    return res.status(400).send({ error: error.details[0].message });
+  }
+
+  //Check if this user already exists
+  const currentUser = await User.findOne({ email: req.body.email });
+  if (currentUser) {
+    return res
+      .status(400)
+      .send({ error: "A user with this email already exists." });
+  }
+
+  //Proceed with creating new user
   const newUser = new User({
     ...req.body,
   });
 
   try {
+    //Password is now hashed in the User model on save.
+    //const salt = await bcrypt.genSalt(parseInt(process.env.SALT_WORK_FACTOR));
+    //newUser.password = await bcrypt.hash(newUser.password, salt);
     const result = await newUser.save();
 
-    return res.status(200).send(result);
+    return res.status(200).send({ _id: result._id, email: result.email });
   } catch (error) {
     for (field in error.errors) {
       dbDebugger(error.errors[field].message);
     }
-    //Check for duplicates in the database.
+    //Check if the error is due to duplicates in the database.
     let errorMessage;
     error.code === 11000
       ? (errorMessage =
--- models/User.js
+++ models/User.js
@@ -1,17 +1,35 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
-const SALT_FACTOR = 10;
+const saltFactor = parseInt(process.env.SALT_WORK_FACTOR);
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, match: /.+\@.+\..+/, unique: true },
-  password: { type: String, required: true, minlength: 3, maxlength: 30 },
+  email: {
+    type: String,
+    required: true,
+    match: /.+\@.+\..+/,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: { type: String, required: true, minlength: 5, maxlength: 1024 },
   dateAdded: { type: Date, default: Date.now },
 });
 
+function validateUser(user) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(user);
+}
+
 userSchema.pre("save", async function save(next) {
   try {
-    const salt = await bcrypt.genSalt(SALT_FACTOR);
+    const salt = await bcrypt.genSalt(saltFactor);
     this.password = await bcrypt.hash(this.password, salt);
     return next;
   } catch (error) {
@@ -22,4 +40,4 @@ userSchema.pre("save", async function save(next) {
 const User = mongoose.model("User", userSchema);
 
 exports.User = User;
-exports.userSchema = userSchema;
+exports.validateUser = validateUser;
