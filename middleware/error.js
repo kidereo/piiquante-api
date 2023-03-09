d67b49066b1af07fb22718e656044dcea363d2d7
kidereo <kirill.derevenski@gmail.com>
2023-03-09 22:21:28 +0100

    Added test for error middleware.

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,8 @@
 const winston = require("winston");
+const express = require("express");
+const app = express();
 
 module.exports = function (err, req, res, next) {
-  winston.error(err.message, err);
+  if (app.get("env") !== "test") winston.error(err.message, err);
   return res.status(500).send({ error: err.message });
 };
---
+++ tests/unit/middleware/error.test.js
@@ -0,0 +1,22 @@
+const error = require("../../../middleware/error");
+
+describe("error middleware ", () => {
+  test("should return the 500 status, an error message and abort without calling next()", () => {
+    const err = {
+      message: "Fatal error",
+      code: 500,
+    };
+    const req = {};
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+    const next = jest.fn();
+
+    error(err, req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({ error: err.message });
+    expect(next).not.toHaveBeenCalled();
+  });
+});
